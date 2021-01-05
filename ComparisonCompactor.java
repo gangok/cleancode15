8ab8eb8e3257e16311145d186a26b80b985094e6
gangok <kstylee@gmail.com>
2021-01-06 08:12:39 +0900

    10th change findCommonPrefixAndSuffix() 함수 정리  - suffixIndex는 실제로는 접미어 길이라는 사실이 드러나서 이름을 변경  - 의미를 분명하게 하기 위해 함수 캡슐화

--- ComparisonCompactor.java
+++ ComparisonCompactor.java
@@ -64,14 +64,21 @@ public class ComparisonCompactor {
 
     private void findCommonPrefixAndSuffix() {
         findCommonPrefix();
-        int expectedSuffix = expected.length() - 1;
-        int actualSuffix = actual.length() - 1;
-        for (; actualSuffix >= prefixIndex && expectedSuffix >= prefixIndex; actualSuffix--, expectedSuffix--) {
-            if (expected.charAt(expectedSuffix) != actual.charAt(actualSuffix)) {
+        int suffixLength = 1;
+        for (; !suffixOverlapsPrefix(suffixLength); suffixLength++) {
+            if (charFromEnd(expected, suffixLength) != charFromEnd(actual, suffixLength)) {
                 break;
             }
         }
-        suffixIndex = expected.length() - expectedSuffix;
+        suffixIndex = suffixLength;
+    }
+
+    private char charFromEnd(String s, int i) {
+        return s.charAt(s.length()-i);
+    }
+
+    private boolean suffixOverlapsPrefix(int suffixLength) {
+        return actual.length() - suffixLength < prefixLength || expected.length() - suffixLength < prefixLength;
     }
 
     private String computeCommonPrefix() {
