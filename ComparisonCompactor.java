9c9fa621226266f177739be982ac8908c519a1a5
gangok <kstylee@gmail.com>
2021-01-06 07:46:07 +0900

    6th change 압축하는 부분을 별도 method로 extract  - 예상 문자열과 실제 문자열을 진짜로 압축하는 if문 안의 코드를 빼내서 compactExpectedAndActual() 생성  - 형식을 맞추는 작업은 formatCompactedComparison()에게 맡김  - compactedExpected와 compactActual을 멤버 변수로 승격

--- ComparisonCompactor.java
+++ ComparisonCompactor.java
@@ -11,6 +11,8 @@ public class ComparisonCompactor {
     private String actual;
     private int prefix;
     private int suffix;
+    private String compactExpected;
+    private String compactActual;
 
     public ComparisonCompactor(int contextLength, String expected, String actual) {
         this.contextLength = contextLength;
@@ -21,10 +23,7 @@ public class ComparisonCompactor {
     @SuppressWarnings("deprecation")
     public String formatCompactedComparison(String message) {
         if (canBeCompacted()) {
-            findCommonPrefix();
-            findCommonSuffix();
-            String compactExpected = compactString(expected);
-            String compactActual = compactString(actual);
+            compactExpectedAndActual();
             return Assert.format(message, compactExpected, compactActual);
         } else {
             return Assert.format(message, expected, actual);
@@ -34,6 +33,13 @@ public class ComparisonCompactor {
 
     private boolean canBeCompacted() {
         return expected != null && actual != null && !areStringsEqual();
+    }
+
+    private void compactExpectedAndActual() {
+        findCommonPrefix();
+        findCommonSuffix();
+        compactExpected = compactString(expected);
+        compactActual = compactString(actual);
     }
 
     private String compactString(String source) {
